cd2f3e248e827f385954470a7ee7ce751d6de33e
Vezio Cantador <veziocantador@yahoo.com.br>
2024-10-01 17:59:23 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "784512", modelo: "Modelo Teste", imei: "123456789", memoria: "64");
+nokia.Ligar();
+nokia.InstalarAplicativo("Netflix");
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "124578", modelo: "Teste Modelo", imei: "987654321", memoria: "128");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Sky");
+iphone.Ligar();
